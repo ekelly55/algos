3b0bef41de1321c4ef3a5a1dfe347fda831beec1
ekelly55 <eamonn.michael.kellyr@email.com>
2022-11-09 22:56:27 -0500

    still not working

--- merge-2-lists.js
+++ merge-2-lists.js
@@ -2,31 +2,19 @@
 
 
 function mergeTwoLists(list1, list2){
-    let list3 = []
+    let list3 = list1
     console.log(list3)
-    if(list2[0] >= list1[list1.length-1]){
-        for(i=0; i<list2.length; i++){
-            list3.push(list2[i])
-        }
-    }
-    if(list2[list2.length-1] <= list1[0]){
-        for(i=0; i<list2.length; i++){
-            list3.unshift(list2[i])
-        }
-    } else {
     for(i=0; i<list2.length; i++){
-        list3.push(list2[i])
-        for(j=0; j<list1.length; j++)
-            if(list2[i] >= list1[j]){
-                list3.push(list2[i])
+        for(j=list3.length-1; j>=0; j--){
+            if(list2[i] <= list3[j]){
+                list3.splice(list3[j-1], 0, list2[i])
                 console.log(list3)
             } else {
-                list3.push(list1[j])
+                list3.push(list2[i])
                 console.log(list3)
             }
         }
     }
-    console.log(list3)
     return list3
 }
 
