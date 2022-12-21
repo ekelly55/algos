c0b8d46d56db389cd4ed3243c971afe9bb51e943
ekelly55 <eamonn.michael.kellyr@email.com>
2022-12-21 12:02:39 -0500

    merge2 is unsolvable in js on leetcode's terms

--- merge-2-lists.js
+++ merge-2-lists.js
@@ -1,105 +1,48 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
 //  You are given the heads of two sorted linked lists list1 and list2.
 
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 // Return the head of the merged linked list.
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
- */
+*/
+
+var mergeTwoLists = function(l1, l2) {
+    function ListNode(val, next) {
+         this.val = (val===undefined ? 0 : val)
+         this.next = (next===undefined ? null : next)
+     }
+     const result = new ListNode(null);
+     let n = result;
+   
+     while (l1 !== null || l2 !== null) {
+       if (l1 === null) {
+         n.next = l2;
+         l2 = l2.next;
+       } else if (l2 === null) {
+         n.next = l1;
+         l1 = l1.next;
+       } else if (l1.val < l2.val) {
+         n.next = l1;
+         l1 = l1.next;
+       } else {
+         n.next = l2;
+         l2 = l2.next;
+       }
+       n = n.next;
+     }
+   
+     return result.next;
+   };
 
 
-function mergeTwoLists(list1, list2){
+let l1=[1, 2, 3]
+let l2 = [1, 3, 4]
 
+console.log(mergeTwoLists(l1, l2))
 
-
-    console.log(list1)
-    console.log(list2)
-
-    class LinkedList {
-        constructor(head){
-            this.head = null
-            this.tail = null
-            this.length = 0
-        }
-        push(val){
-            let newNode = new ListNode(val)
-            if(!this.head){
-                this.head = newNode
-                this.tail = this.head
-            } else {
-                this.tail.next = newNode
-                this.tail = newNode
-            }
-            this.length ++
-            return this
-        }
-        pop(){
-            if(!this.head) return undefined
-            let current = this.head;
-            let newTail = current;
-            while(current.next){
-                newTail = current;
-                current = current.next;
-            }
-            this.tail = newTail
-            this.tail.next = null;
-            this.length --;
-            if(this.length === 0){
-                this.head = null;
-                this.tail = null;
-            }
-            return current
-        }
-    }
-    class ListNode {
-        constructor(val) {
-            this.val=val,
-            this.next=null
-        }
-    }
-
-    let output = new LinkedList(null)
-    let i = 0
-    if(list1.length && list2.length){
-
-        while(i <= list1.length || i <= list2.length){
-            console.log(`list1 length is ${list1.length}, list2 length is ${list2.length}`)
-            console.log(`loop number ${i+1}`)
-            if(list1[i] <= list2[i]){
-                output.push(list1[i])
-                output.push(list2[i])
-            } else if(list1[i]>list2[i]){
-                output.push(list2[i])
-                output.push(list1[i])
-            }
-            ++i
-        }
-    } else if(list1.length && !list2.length){
-        while(i <= list1.length){
-            console.log(`list1 length is ${list1.length}`)
-            console.log(`loop number ${i+1}`)
-          
-            output.push(list1[i])
-            ++i
-        }
-    } else if(list2.length && !list1.length){
-        while(i <= list2.length){
-
-            output.push(list2[i])
-            ++i
-        }
-    }
-    console.log(output.head)
-    return output.head
-      
-}
-
-mergeTwoLists([], [])
+//this is unsolvable in JS on these terms.
