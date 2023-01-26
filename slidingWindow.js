ed5bbc4110b379acb0d3075e7a79a0ab0c49cf3e
ekelly55 <eamonn.michael.kellyr@email.com>
2023-01-26 13:39:05 -0500

    brain melt. lunch break

--- slidingWindow.js
+++ slidingWindow.js
@@ -60,7 +60,63 @@ function maxSubArraySum(arr, num){
     return sum
 }
 
-let arr = [1, 1, 9, 5, -10, 2]
-let n = 4
+let arr = [0, 1, -8, 15, -10, 12]
+let n = 1
 
-console.log(maxSubArraySum(arr, n))
+//console.log(maxSubArraySum(arr, n))
+
+
+//takes an arr of posi int and a posi int target. 
+//return the minimum length of a contiguous subarray in which the subarray sum is >=target. if none, return zero, because there is no length that will satisfy it. 
+function minSubArrayLen(arr, n){
+
+    
+    
+    //now declare our window pointer indexes and starting sum
+    let start = 0
+    console.log(`start index is ${start}`)
+    let end = 1
+    console.log(`end index is ${end}`)
+    let subLength
+    let sum = arr[start] + arr[end]
+    console.log(`sum is ${sum}`)
+    //need a temp sum to store current best while we look for other
+    let tempSum = 0
+    //calculate tempSum as total sum of array
+    for(int of arr){
+        tempSum += int
+    }
+    
+    //now do a while loop, expanding and shifting the window as necessary, and checking if n exists in the arr
+    while(start < end < arr.length){
+        // console.log("running")
+        if(arr[start] >= n || arr[end] >= n){
+            return 1
+        } 
+        //add until sum >= n
+        if(sum < n){
+            
+            console.log(`sum is smaller than n, extend the window`)
+            end++
+            console.log(`new end index is ${end}`)
+            sum += arr[end]
+            
+            console.log(` new sum is ${sum}`)
+            subLength = start + end + 1
+            console.log(` subarray length is ${subLength}`)
+            
+            console.log(` sum is is ${sum}`)
+            
+        } 
+        //now set tempSum if it's closer than tempSum
+        if(num <= sum < tempSum){
+            tempSum = sum
+        }
+        //now increment the start to see if we can shrink the window. if we can't, 
+    }
+//we know we're gonna return the diff between the pointers
+    console.log(`the min length is ${subLength}`)
+    return subLength
+}
+
+minSubArrayLen([2,1,6,5,4], 9)
