10004f7798516cd0dd5c72952f245771a1c95e03
ekelly55 <eamonn.michael.kellyr@email.com>
2023-02-09 10:46:59 -0500

    solved recursive palindrome

--- recursion2.js
+++ recursion2.js
@@ -15,11 +15,28 @@ function reverse(str){
     return str[str.length - 1] + reverse(str.slice(0, (str.length - 1) ))
 }
 
-console.log(reverse(""))
+// console.log(reverse(""))
 
-let str = "rats"
 
-// console.log(str.slice(0, (str.length - 1) ))
+//palindromes: can the string be empty? probably have to account for that. conditionals don't do well with str.length === 0, and it's not !str
 
+function isPalindrome(str){
+    //it should basically be the last function, and it just checks if they're the same. do we want to bother stopping it early if it's not a match? what is the runtime like cpompared w/ a loop?
+    function reverse(input){
+
+        if(input === ""){
+            return undefined
+        }
+        if(input.length === 1){
+            return input[0]
+        }
+        return (input[input.length - 1] + reverse(input.slice(0, (input.length - 1) )))
+    }
+    return (reverse(str) === str)
+        
+}
+
+
+console.log(isPalindrome(""))
 
 
